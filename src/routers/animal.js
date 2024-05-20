a32900e5ef6bfc39b29da0437ff548424e28f28f
Katherine Ortiz <katherinem.ortizb@konradlorenz.edu.co>
2024-05-20 16:01:54 -0500

    Primer Commit
    Se agrego el archivo .env y se corrio la API.

--- src/routers/animal.js
+++ src/routers/animal.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
-const verifyToken = require('./validate_token');
+
 //Nuevo animal
 router.post("/animals", (req, res) => {
     const animal = animalSchema(req.body);
@@ -11,7 +11,7 @@ router.post("/animals", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
-router.get("/animals", verifyToken, (req, res) => {
+router.get("/animals", (req, res) => {
     animalSchema.find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
